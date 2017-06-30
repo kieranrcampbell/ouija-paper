906f9885e8080917283f7a2659c2d70de74a10e8
Kieran Campbell <kieranrcampbell@gmail.com>
2017-06-30 12:10:50 +0100

    weird removal of ouija and dpt functions

--- scripts/pseudotime_fitting_functions.R
+++ scripts/pseudotime_fitting_functions.R
@@ -68,6 +68,53 @@ fit_pc1_pseudotime <- function(sce) {
 }
 
 
+fit_ouija_pseudotime <- function(sce, marker_only = TRUE, k_mean = NULL,
+                                 iter = 10000, inference_type = "hmc") {
+  ouija_pseudotime <- NULL
+  
+  if(marker_only) {
+    is_marker_gene <- fData(sce)$is_marker
+    k_mean <- fData(sce)$k_mean[is_marker_gene]
+    k_sd <- fData(sce)$k_sd[is_marker_gene]
+    if(any(is.na(k_sd))) k_sd <- NULL
+    oui <- tryCatch(ouija(sce[is_marker_gene, ], strengths = k_mean, strength_sd = k_sd,
+                          iter = iter, inference_type = inference_type),
+                    error = function(e) {
+                      message(paste("Ouija error!"))
+                      return(NA)
+                    })
+    if(is.na(oui)) {
+      return(rep(NA, ncol(sce)))
+    }
+    ouija_pseudotime <- map_pseudotime(oui)
+  } else {
+    oui <- tryCatch(ouija(sce, strengths = k_mean,
+                          iter = iter, inference_type = inference_type),
+                    error = function(e) {
+                      message(paste("Ouija error!"))
+                      return(NA)
+                    })
+    if(is.na(oui)) {
+      return(rep(NA, ncol(sce)))
+    }    
+    ouija_pseudotime <- map_pseudotime(oui)
+  }
+  return(ouija_pseudotime)
+}
+
+
+fit_dpt_pseudotime <- function(sce) {
+  # we need to add a small amount of noise to DPT in order
+  # to avoid the 'non-unique' error
+  gene_sds <- matrixStats::rowSds(exprs(sce))
+  gene_collapse_sd <- 1e-6 * gene_sds
+  new_exprs <- apply(exprs(sce), 2, function(x) rnorm(length(x), x, gene_collapse_sd))
+  
+  ts <- Transitions(t(new_exprs))
+  pt <- dpt(ts, branching = FALSE)  
+  return(pt$DPT)
+}
+
 
 
 
